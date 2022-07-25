a0615f19493ffdd0e01382d8e79235ccca27dc4b
Victor Barros <victorjuda49@gmail.com>
2022-07-25 00:07:51 -0300

    Finalizando servidor

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3050, () => 
-console.log('Servidor iniciado na porta 3050')
+app.listen(3000, () => 
+console.log('Servidor iniciado na porta 3000')
+);
+
+app.get('/', (req, res) => 
+res.json({
+    "Status":200,
+    alive:true
+})
 );
