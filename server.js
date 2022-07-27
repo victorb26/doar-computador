1d98a61048dee32e51639abee92f7ac942933463
Victor Barros <victorjuda49@gmail.com>
2022-07-27 18:07:36 -0300

    reorganizando as pastas e os arquivos

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-import app from "./app.js"
+import app from "./src/routes/index.js";
 
 app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,6 @@
 import express from "express";
-import donates from "./donateRoute.js";
-
-const  routes = app => {
+const app = express();
 
 app.get("/", (req, res) => res.status(200).json({ alive: true }));
 
-app.use(
-    express.json(),
-    donates
-    
-)
-} 
-
-export default routes;
+export default app;
