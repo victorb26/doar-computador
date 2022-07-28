396634ceb886e8e562418f818fc1bb2a005294a3
Victor Barros <victorjuda49@gmail.com>
2022-07-28 11:05:26 -0300

    implementando controller

--- src/controllers/donationController.js
+++ src/controllers/donationController.js
@@ -1,1 +1,9 @@
 import DonationService from "../services/donationService";
+exports.post = async (req, res) => {
+  try {
+    const createDonate = await DonationService.postDonate(req.body);
+    res.status(201).json(createDonate({sucess:true}));
+  } catch (error) {
+    return res.status(400).json({ error: true });
+  }
+};
