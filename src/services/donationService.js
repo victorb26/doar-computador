1acfd1fbd0d5711cd8e6333ea3dd38bac717ce0e
Victor Barros <victorjuda49@gmail.com>
2022-07-28 10:41:36 -0300

    criando Services

---
+++ src/services/donationService.js
@@ -0,0 +1,27 @@
+import Donate from "../models/donationModel.js";
+
+export default  class DonationService{
+    static async addDonate(data){
+        try{
+            const newDonate = {
+                name:data.name,
+                email:data.email,
+                phone:data.phone,
+                zip:data.zip,
+                city:data.city,
+                state:data.state,
+                streetAddress:data.streetAddress,
+                number:data.number,
+                complement:data.complement,
+                neighborhood:data.neighborhood,
+                deviceCount:data.deviceCount,
+                devices: [
+                    {type:data.type, condition:data.condition},
+                ]
+            };
+            const response = await new Donate(newDonate).save();
+        }catch (error){
+            
+        }
+    }
+};
