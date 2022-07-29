6fc0518cf66df677e4efcf57427111aff31425a4
Victor Barros <victorjuda49@gmail.com>
2022-07-29 12:10:10 -0300

    adc validação de email

--- src/controllers/donationController.js
+++ src/controllers/donationController.js
@@ -1,4 +1,6 @@
 import Donate from "../models/donationModel.js";
+import pkg from "express-validator";
+const {body, validationResult} = pkg;
 import db from "../infra/db.js";
 
 const donationController = (app) => {
@@ -6,13 +8,34 @@ const donationController = (app) => {
     res.status(200).json({ alive: true });
   });
 
-  app.post("/donation", (req, res) => {
+  app.post("/donation",[
+    body("email").isEmail()
+  ], (req, res) => {
     res.status(200);
     const body = req.body;
-    console.log(body);
     const donate = new Donate(body.nome, body.email, body.senha);
     db.donate.push(donate);
-    res.json({ "Produto cadastrado": donate });
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({
+        "error": true,
+        "requiredFields": [
+            "name",
+            "phone",
+            "zip",
+            "city",
+            "state",
+            "streetAddress",
+            "number",
+            "complement",
+            "neighborhood",
+            "deviceCount",
+            "devices"
+        ],
+        "errorMessage": "Todos os campos obrigatórios devem ser informados"
+    })
+    }
+    res.json({ sucess:true });
   });
 };
 
