d104d694dfa0ed0e23498fe9a3fb0b0f5ebe7218
Victor Barros <victorjuda49@gmail.com>
2022-07-30 14:13:54 -0300

    validando devices

--- src/controllers/donationController.js
+++ src/controllers/donationController.js
@@ -1,6 +1,6 @@
 import Donate from "../models/donationModel.js";
 import pkg from "express-validator";
-const { body, validationResult } = pkg;
+const { check, validationResult } = pkg;
 import db from "../infra/db.js";
 
 const donationController = (app) => {
@@ -8,42 +8,72 @@ const donationController = (app) => {
     res.status(200).json({ alive: true });
   });
 
-  app.post("/donation",
-  [body("email").isEmail().normalizeEmail(),
-  body("phone").isLength({min:10, max:11}),//10 para número fixo e 11 para celulares
-  body("zip").isLength(8),
-  body("deviceCount").isNumeric(),
-  body("devices")], (req, res) => {
-    res.status(200);
+  const condition = [
+    "working",
+    "notworking",
+    "broken",
+    ];
+  const type =[
+    "notebook",
+    "desktop",
+    "netbook",
+    "screen",
+    "printer",
+    "scanner"
+  ];
 
-    const body = req.body;
+  app.post(
+    "/donation",
+    [
+      check("email").isEmail().normalizeEmail(),
+      check("phone").isLength({ min: 10, max: 11 }), //10 para número fixo e 11 para celulares
+      check("zip").isLength(8),
+      check("deviceCount").isInt(),
+      check("devices").isIn([{type, condition}])
+    ],
+    (req, res) => {
+      const body = req.body;
 
-    const donate = new Donate(body.name, body.email, body.phone, body.zip, body.city, body.state, body.streetAddress, body.number, body.complement, body.neighborhood, body.deviceCount, body.devices);
-    db.donate.push(donate);
+      const donate = new Donate(
+        body.name,
+        body.email,
+        body.phone,
+        body.zip,
+        body.city,
+        body.state,
+        body.streetAddress,
+        body.number,
+        body.complement,
+        body.neighborhood,
+        body.deviceCount,
+        body.devices
+      );
+      db.donate.push(donate);
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        error: true,
-        requiredFields: [
-          "name",
-          "email",
-          "phone",
-          "zip",
-          "city",
-          "state",
-          "streetAddress",
-          "number",
-          "complement",
-          "neighborhood",
-          "deviceCount",
-          "devices",
-        ],
-        errorMessage: "Todos os campos obrigatórios devem ser informados",
-      });
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: true,
+          requiredFields: [
+            "name",
+            "email",
+            "phone",
+            "zip",
+            "city",
+            "state",
+            "streetAddress",
+            "number",
+            "complement",
+            "neighborhood",
+            "deviceCount",
+            "devices",
+          ],
+          errorMessage: "Todos os campos obrigatórios devem ser informados",
+        });
+      }
+      res.status(200).json({ sucess: true });
     }
-    res.json({ sucess: true });
-  });
+  );
 };
 
 export default donationController;
