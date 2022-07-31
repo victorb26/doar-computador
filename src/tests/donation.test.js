63433ad9f411093d965d1c53ff6019b8fd318ee1
Victor Barros <victorjuda49@gmail.com>
2022-07-31 19:06:40 -0300

    teste do email inválido

--- src/tests/donation.test.js
+++ src/tests/donation.test.js
@@ -3,33 +3,60 @@ const express = require("express");
 const app = express();
 
 describe('POST /users test', ()=> {
-    const data = {
-        "name":"",
-        "email":"email@email.com",
-        "phone":"",
-        "zip":"23230230",
-        "city":"Polis",
-        "state":"PL",
-        "streetAddress":"Polis",
-        "number":"23",
-        "complement":"Fundos",
-        "neighborhood":"Polis",
-        "deviceCount":1,
-        "devices":
-                [
-                    {"type":"notebook","condition":"working"},
-                ]   
-    
-    };
+  
     it('Should the return Status(400) and (errorMenssage)',  () => {
+        const data = {
+            "name":"",
+            "email":"email@email.com",
+            "phone":"",
+            "zip":"23230230",
+            "city":"Polis",
+            "state":"PL",
+            "streetAddress":"Polis",
+            "number":"23",
+            "complement":"Fundos",
+            "neighborhood":"Polis",
+            "deviceCount":1,
+            "devices":
+                    [
+                        {"type":"notebook","condition":"working"},
+                    ]   
+        
+        };
+
       request(app)
         .post('/donation')
         .send(data)
         .expect('errorMessage')
         .expect(400)
-        .end(function (err, res) {
-            if (err) throw err;
-          });
+    });
+
+    it('Should return Status(400) and body {error: true, errorMessage: "E-mail invalido"}',  () => {
+       
+        const data = {
+            "name":"Victor",
+            "email":"emailemail.com",
+            "phone":"21982293270",
+            "zip":"23230230",
+            "city":"Polis",
+            "state":"PL",
+            "streetAddress":"Polis",
+            "number":"23",
+            "complement":"Fundos",
+            "neighborhood":"Polis",
+            "deviceCount":1,
+            "devices":
+                    [
+                        {"type":"notebook", "condition":"working"},
+                    ]   
+        
+        };
+
+        request(app)
+        .post('/donation')
+        .send(data)
+        .expect({error: true, errorMessage: "E-mail invalido"})
+        .expect(400)
     });
 
 
