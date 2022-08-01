9e62b7d26c7348d85326ec456b6c5028381ee7b2
Victor Barros <victorjuda49@gmail.com>
2022-07-31 22:01:28 -0300

    make it better 5

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 import app from "./src/app.js"
 
-app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`servidor escutando em http://localhost:${port}`)
+})
